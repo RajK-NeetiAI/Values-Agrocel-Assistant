73ca721ad068858dbd2e48eadc33481a65e2456a
RajK-NeetiAI <raj@neeti.ai>
2023-12-22 15:07:34 +0530

    fixing cors

--- run.py
+++ run.py
@@ -9,12 +9,7 @@ import config
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+app.add_middleware(CORSMiddleware)
 
 
 @app.get('/')
